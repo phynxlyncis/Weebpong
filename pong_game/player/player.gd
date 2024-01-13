4337fd8633aca0bd73429db749de34a117a86867
torgeir.skovholt1@gmail.com <torgeir.skovholt1@gmail.com>
2024-01-13 15:34:40 +0100

    Først commit

---
+++ pong_game/player/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+# var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Handle jump.
+	# if Input.is_action_just_pressed("ui_accept") and is_on_fl:
+	# 	velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	# var direction = Input.get_axis("ui_left", "ui_right")
+	# if direction:
+	# 	velocity.x = direction * SPEED
+	# else:
+	# 	velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	
+
+	move_and_slide()
