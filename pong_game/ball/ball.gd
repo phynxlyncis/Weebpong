4b454bec6fc6d225791823399dd32a56ea43c29d
phynxlyncis <phynxlyncis@hotmail.com>
2024-01-13 18:39:33 +0100

    Walls

--- pong_game/ball/ball.gd
+++ pong_game/ball/ball.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var SPEED = 300.0
+var NEW_SPEED = 500.0
+var speed_increase = false
 
 func _ready():
 	var random_vector = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
@@ -15,6 +17,11 @@ func _physics_process(delta):
 
 func _on_collide_body_entered(body):
 	if body.name == "Player" || body.name == "Player2":
+		if not speed_increase:
+			speed_increase = true
+			var vec_direction = velocity.normalized()
+			velocity.x = vec_direction[0]*NEW_SPEED
+			velocity.y = vec_direction[1]*NEW_SPEED
 		velocity.x = (-1) * velocity.x
 	elif body.name == "Wall" || body.name == "Wall2":
 		velocity.y = (-1) * velocity.y
